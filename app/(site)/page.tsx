b6c7901bf7f3e0cb79a87194758dbd0211106b94
Sebastian Rojas <sebastianrojas812@gmail.com>
2023-06-02 15:07:17 -0400

    building UI, header, button, box, sidebar components

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,29 @@
+import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
   return (
-    <div className="text-green-500">hey spotify!</div>
+    <div className="w-full h-full overflow-hidden rounded-lg bg-neutral-900 overlow-y-auto">
+      <Header>
+        <div className="mb-2">
+          <h1 className="text-3xl font-semibold text-white">
+            Welcome back
+          </h1>
+          <div className="grid grid-cols-1 gap-3 mt-4 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+            <ListItem name='Liked Songs' href='liked' image='/images/liked.png' />
+          </div>
+        </div>
+      </Header>
+      <div className="px-6 mt-2 mb-7">
+        <div className="flex items-center justify-between">
+          <h1 className="text-2xl font-semibold text-white">
+            Newest songs
+          </h1>
+        </div>
+        <div>
+          List of Songs!
+        </div>
+      </div>
+    </div>
   )
 }
