d3c66c8a5755e84d373f880a169f929399076d78
Sebastian Rojas <sebastianrojas812@gmail.com>
2023-06-29 12:22:19 -0400

    typo in helper file which caused incorrect redirect with stripe

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -3,7 +3,7 @@ import { Price } from "@/types";
 export const getUrl = () => {
     let url =
         process.env.NEXT_PUBLIC_SITE_URL ??
-        process.env.NEXT_PUBLIC_VERCEL_URLL ??
+        process.env.NEXT_PUBLIC_VERCEL_URL ??
         'http://localhost:3000/';
 
     url = url.includes('http') ? url : `https://${url}`;
